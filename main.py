4cf2f502f4f996e612ea504b6eb9387f314dded4
Breval Le Floch <breval2000@live.fr>
2021-09-15 21:55:12 +0200

    useradd ( add to use autoincrement)

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from endpoints import *
 
 db.create_all()
+users = User( first_name="Andy", last_name="Cinquin", email="andy.cinquin@gmail.com", password="123")
+db.session.add(users)
+db.session.commit()
 
 if __name__ == "__main__":
+
 	app.run(debug=True)
-	users = User(id=2, first_name="Breval", last_name="Le Floch", email="breval2000@live.fr", password="123"), \
-	        User(id=3, first_name="Mathis", last_name="Gautier", email="mathis@gmail.com", password="123"), \
-	        User(id=4, first_name="Andy", last_name="Cinquin", email="andy.cinquin@gmail.com", password="123"), \
-	        User(id=4, first_name="Test", last_name="Test", email="Test@test", password="123")
-	db.session.add_all(users)
-	db.session.commit()
