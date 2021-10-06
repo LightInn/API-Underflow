48071181878ccc1ca270ae87f4d9ff9ee58a7b24
Mathis <math44100@yahoo.fr>
2021-10-06 11:07:35 +0200

    tiens bréval LEF LOCH OK????

--- conf.py
+++ conf.py
@@ -12,6 +12,7 @@ app.config['SECRET_KEY'] = 'fc036e4bc6974f92bb6577ae886ae113'
 app.config['TOKEN_KEY'] = 'a12b1c64bc584e7595e02830bd4bb981'
 app.config['WTF_CSRF_SECRET_KEY'] = '2326321d3ad54bcfaafa33dd59cd2e26'
 app.config['WTF_CSRF_CHECK_DEFAULT'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 csrf = CSRFProtect()
 csrf.init_app(app)
